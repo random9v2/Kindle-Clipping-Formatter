5e7237c7489f82a3c07a25908cb5d985277ed7be
maw101 <maw101@aber.ac.uk>
2020-06-05 10:36:46 +0100

    Add required imports and expected constants

---
+++ src/kindle_clipping_html_formatter.py
@@ -0,0 +1,11 @@
+import re
+import os
+from datetime import datetime
+
+from kindle_clipping_html_templates import PAGE, HIGHLIGHT
+
+CLIPPINGS_FILENAME = "My Clippings.txt"
+HTML_FILE_EXTENSION = ".html"
+OUTPUT_DIRECTORY_NAME = 'output'
+HIGHLIGHT_SEPARATOR = "=========="
+
